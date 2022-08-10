e5ad762ee61f88fbaff606e988fe9c31698fb646
Usama Javaid <usama.javaid@brainxtech.com>
2022-08-11 04:25:17 +0500

    custom js added

--- javascripts/discourse/initializers/init-topic-excerpts.js
+++ javascripts/discourse/initializers/init-topic-excerpts.js
@@ -6,3 +6,7 @@ export default apiInitializer("0.8", (api) => {
     expandPinned: true,
   });
 });
+let customCategoryCard = document.getElementsByClassName('above-discovery-categories-outlet')[0];
+if (!customCategoryCard){
+  customCategoryCard.parentElement.classList.add('list-area-section');
+}
